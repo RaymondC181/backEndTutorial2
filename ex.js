804f6801b74bd4e3dbca27f476ebf76f27be34c1
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2023-01-21 11:41:46 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -34,16 +34,61 @@ app.post('/api/courses', (req, res)=> {
 
 //you write the if code here
 //add an if statment so that the name of the course you post is min(3) characters
+if(course.length>=3)
+{
     const courses = {
         //we assign an ID and a name property
         id:courses.length + 1,
         name:req.body.name
     }
+}
         // YOU WRITE THE NEXT LINES OF CODE
         //next step: push it to the array
+        courses.push(req);
         //next step: the server should return the new resource to the client in the body of the response 
+        res.send(courses);
 });
 
 app.listen(3000, () => {
     console.log('Listening on port 3000 ...')
 })
+
+//here we need the specific id of the course we want to update 
+app.put('/api/courses/:id', (req, res)=> {
+    //write code in order to look up the course, if not existing, return a 404
+    if(courses.indexOf(id)<0)
+    {
+        return "404"; 
+    }
+
+    //otherwise
+    else{
+        //update the course
+        courses = courses; 
+        //return the updated course  
+        return courses; 
+    }
+}); 
+
+app.delete('/api/courses/:id', (req, res)=>{
+    //code the following logic
+    
+    //look up the course by id
+    var result = $.grep(courses, function(req){ return req.id == id; });
+    if(result.length === 0){
+        return "404"; 
+    }
+    else{
+        var index = list.map(x => {
+            return x.Id;
+          }).indexOf(id);
+          
+          list.splice(index, 1);
+          return "course is deleted"; 
+    }
+        //return the 404 if does not exist
+        //delete the course by index HINT: use the indexOf() and 
+        //splice() methods
+        //return the response to the client the course that was deleted
+
+});
